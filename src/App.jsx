bfded03a34c2c14994d3df97327b1ae380ec72f1
BeerCSIM-AIT <st121032@ait.ac.th>
2024-08-21 08:58:42 +0700

    edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Products from './pages/Products';
 import Contact from './pages/Contact';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
@@ -16,7 +16,7 @@ const App = () => {
         <Route path="/contact" element={<Contact/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
