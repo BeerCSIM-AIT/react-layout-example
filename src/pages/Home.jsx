6dc3760470a7def013ea18250687a12170401b7f
BeerCSIM-AIT <st121032@ait.ac.th>
2024-08-21 11:13:39 +0700

    edit home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,19 @@ const Home = () => {
     <Layout>
       <h1 className="font-bold text-4xl text-blue-950">Home</h1>
       <hr />
-      <p className="mt-8 mb-20 text-blue-950">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
-        accusantium adipisci magnam in nesciunt suscipit sed doloribus vero
-        rerum asperiores distinctio corrupti quisquam fuga dolore, tempora ad
-        labore deserunt perferendis?
-      </p>
+      <a
+        href="#"
+        className="block max-w-sm mt-5 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </a>
+      
     </Layout>
   );
 };
